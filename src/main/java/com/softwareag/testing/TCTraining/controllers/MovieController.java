7113a18f9b093c6c0451f7f6b5636b2fbd4f98a7
RAEO <RAEO@SAG-BVPGLH2.eur.ad.sag>
2019-08-07 12:06:23 -0300

    Persistence and Lock Handling

--- src/main/java/com/softwareag/testing/TCTraining/controllers/MovieController.java
+++ src/main/java/com/softwareag/testing/TCTraining/controllers/MovieController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.softwareag.testing.TCTraining.models.Movie;
 import com.softwareag.testing.TCTraining.services.MovieService;
 
-import net.sf.ehcache.Element;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 
 @RestController
 @RequestMapping("/movie")
@@ -26,14 +26,16 @@ public class MovieController {
 	@Autowired
 	private MovieService movieService;
 
-	//O objeto CacheManager manipula um conjunto de caches em uma só JVM
-	//Para criar um CacheManager, usar: CacheManager cacheManager = new CacheManager("ehcache.xml")
-	//Como existe o uso do Spring Boot, o EHCACHE é aderente à especificação JSR-107 e o proprio
-	//Spring consegue construir o CacheManager automaticamente, bastando ao desenvolvedor trabalhar
-	//somente com a injeção de dependencias
+	// O objeto CacheManager manipula um conjunto de caches em uma só JVM
+	// Para criar um CacheManager, usar: CacheManager cacheManager = new
+	// CacheManager("ehcache.xml")
+	// Como existe o uso do Spring Boot, o EHCACHE é aderente à especificação
+	// JSR-107 e o proprio
+	// Spring consegue construir o CacheManager automaticamente, bastando ao
+	// desenvolvedor trabalhar
+	// somente com a injeção de dependencias
 	@Autowired
 	private CacheManager cacheManager;
-
 
 	@RequestMapping(value = "/getMovies", method = RequestMethod.GET, produces = "application/json")
 	public List<Movie> getAllMovies() {
@@ -51,21 +53,66 @@ public class MovieController {
 
 		Movie movie = new Movie(4, "Titanic", "James Cameron");
 
-		Cache cache = cacheManager.getCache("testCache"); //Obtem um cache configurado em EHCACHE.XML
-		cache.put(new Element(1L, movie)); //Guarda um elemento no Cache
+		Cache cache = cacheManager.getCache("testCache"); // Obtem um cache configurado em EHCACHE.XML
+		cache.put(new Element(1L, movie)); // Guarda um elemento no Cache
 
 		return "Elemento " + movie.toString() + " cacheado!";
 
 	}
-	
-	@RequestMapping(value="/getCachedMovie", method=RequestMethod.GET, produces = "application/json")
+
+	@RequestMapping(value = "/getCachedMovie", method = RequestMethod.GET, produces = "application/json")
 	public Movie getCachedMovie() {
+
+		Cache cache = cacheManager.getCache("testCache");
+		Element e = cache.get(1L); // Obtem elemento do Cache
+		Movie cachedMovie = (Movie) e.getObjectValue(); // Obtem o Valor do Elemento cacheado
+
+		return cachedMovie;
+
+	}
+
+	@RequestMapping(value = "/removeAndUpdateCache", method = RequestMethod.GET, produces = "application/json")
+	public Movie updateAndRemove() {
+
+		Cache cache = cacheManager.getCache("testCache");
+		Element e = cache.removeAndReturnElement(1L); //Obtem e limpa o elemento cacheado
+
+		return (Movie) e.getObjectValue();
+
+	}
+	
+	public void lockExample() {
 		
 		Cache cache = cacheManager.getCache("testCache");
-		Element e = cache.get(1L); //Obtem elemento do Cache
-		Movie cachedMovie = (Movie) e.getObjectValue(); //Obtem o Valor do Elemento cacheado
 		
-		return cachedMovie;
+		//Obter Lock de Thread para uma chave
+		cache.acquireReadLockOnKey(1L); //Lock de Leitura
+		cache.acquireWriteLockOnKey(1L); //Lock de Escrita
+		
+		//Verificar se existe Lock de chave na Thread corrente
+		cache.isReadLockedByCurrentThread(1L); //Lock de Leitura
+		cache.isWriteLockedByCurrentThread(1L); //Lock de Escrita
+		
+		//Liberar Lock da Thread Corrente
+		cache.releaseReadLockOnKey(1L);  //Lock de Leitura
+		cache.releaseWriteLockOnKey(1L); //Lock de Escrita
+		
+	}
+	
+	public void persistenceExample() {
+		
+		Cache cache = cacheManager.getCache("testCache");
+		
+		//Os procedimentos abaixo são executados quando uma aplicação é STANDALONE
+		//Em aplicações com cache distribuido, o TSA gerencia automaticamente o
+		//ciclo de vida do cache
+		
+		//Se o cache está configurado como "disk persistent", ao final do
+		//processamento do objeto é necessário descartar o cache
+		cache.dispose(); //Executa o descarte do cache
+		
+		//Executa a limpeza do cache e aloca os dados para o disco
+		cache.flush();
 		
 	}
 
